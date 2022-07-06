175245a2169d1f27bb3870793c12ab7136a21664
ramusini <ramusini@gmail.com>
2022-07-06 07:34:31 +0000

    [Start]first commit

--- db/migrate/20220706062459_devise_create_users.rb
+++ db/migrate/20220706062459_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
+      t.string :postal_code, null: false
+      t.string :prefecture, null: false
+      t.string :city, null: false
+      t.string :age, null: false
 
       ## Recoverable
       t.string   :reset_password_token
