b51a17434ddc728b53da31c461ca2a58661c8c48
ramusini <ramusini@gmail.com>
2022-07-09 10:35:15 +0000

    [Add]検索結果の表示

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,9 +1,15 @@
 class Public::SearchesController < ApplicationController
-  require 'bgg-api'
 
   def search
     bgg = BggApi.new
-    @results = bgg.search( {:query => params[:bg_title], :type => 'boardgame'} )
+    if params[:bg_id].blank?
+      res = bgg.search( {:query => params[:bg_title], :type => 'boardgame'} )
+      # resにあるハッシュのキー名が"item"のものを配列化する。mapでそこから、"id""value"がキー名の配列が入った配列を作成し、@resultsに格納
+      @results = res["item"].map{|item| {id: item["id"], value: item.dig("name", 0, "value")}} #←キー名がnameの値が配列になっているので、そこから0番の値のうち、キー名がvalueのものを抜いている
+    else
+      res = bgg.thing({:id => params[:bg_id]})
+      @results = res
+    end
   end
 
 end
