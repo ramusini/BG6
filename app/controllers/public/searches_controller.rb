43d3ae7790159c5bc9378f56c455c32001731438
ramusini <ramusini@gmail.com>
2022-07-11 08:38:05 +0000

    [Add]検索結果から詳細データをを取得

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,13 +2,16 @@ class Public::SearchesController < ApplicationController
 
   def search
     bgg = BggApi.new
-    if params[:bg_id].blank?
+    if params[:bg_title].present?
+      # トップ画面でユーザーが入力した検索ワードを『:bg_title』として受け受けてsearch。resに格納。
       res = bgg.search( {:query => params[:bg_title], :type => 'boardgame'} )
-      # resにあるハッシュのキー名が"item"のものを配列化する。mapでそこから、"id""value"がキー名の配列が入った配列を作成し、@resultsに格納
-      @results = res["item"].map{|item| {id: item["id"], value: item.dig("name", 0, "value")}} #←キー名がnameの値が配列になっているので、そこから0番の値のうち、キー名がvalueのものを抜いている
+      # resに格納されたハッシュのキー名が"item"のものを抜き出して配列化。
+      # mapメソッドでキー名が『id』『value』の配列の値を抜き出し、新たに配列を作成。@resultsに格納。
+      # 『value』でdigメソッドを利用しているのは、値がなかった場合でもエラーを吐かないようにするため。
+      @results = res["item"].map{|item| {id: item["id"], value: item.dig("name", 0, "value")}} # ←キー名『name』の値が配列になっているので、そこから0番の値のうち、キー名がvalueのものを抜いている
     else
       res = bgg.thing({:id => params[:bg_id]})
-      @results = res
+      @result = res["item"].map{|item| {id: item["id"], image: item["image"], title: item.dig("name", 0, "value"), minplayer: item.dig("minplayers", 0, "value"), maxplayer: item.dig("maxplayers", 0, "value"), playingtime: item.dig("playingtime", 0, "value")}}
     end
   end
 
