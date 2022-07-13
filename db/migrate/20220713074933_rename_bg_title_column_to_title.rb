fd5aeac1b0d81a29b412f2817f4f209cd5bcff72
ramusini <ramusini@gmail.com>
2022-07-13 08:44:50 +0000

    [Add]new,index,edit,destroy機能

---
+++ db/migrate/20220713074933_rename_bg_title_column_to_title.rb
@@ -0,0 +1,6 @@
+class RenameBgTitleColumnToTitle < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :bucket_lists, :bg_title, :bucket_title
+    rename_column :board_games, :bg_title, :played_title
+  end
+end
