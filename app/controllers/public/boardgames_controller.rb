0df5e1ce601327f5f3f212ed1cdf035d993519d8
ramusini <ramusini@gmail.com>
2022-07-28 12:00:57 +0000

    [Fix]bug

--- app/controllers/public/boardgames_controller.rb
+++ app/controllers/public/boardgames_controller.rb
@@ -6,12 +6,12 @@ class Public::BoardgamesController < ApplicationController
 
   def show
     @played_bg = BoardGame.find(params[:id])
-    @records = Record.where(board_game_id: @played_bg.bg_id)
+    @records = Record.where(board_game_id: @played_bg.bg_id, user_id: current_user.id)
   end
 
   def create
     # 遊んだボドゲが未登録の場合保存
-    if BoardGame.where(bg_id: params[:bg_id]).blank?
+    if BoardGame.where(bg_id: params[:bg_id], user_id: current_user.id).blank?
       @new_played_bg = BoardGame.new(boardgame_params)
       @new_played_bg.save
     else
