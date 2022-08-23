9b8c028a21de8984ed9db7c45ca9e29a271a9c7c
ramusini <ramusini@gmail.com>
2022-08-23 13:02:31 +0000

    Rspec homeコントローラー

--- spec/system/tags_spec.rb
+++ spec/system/tags_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 describe '新規タグ作成画面(new_admin_tag_path)のテスト' do
-  let!(:tag) { create(:tag, name:'hoge') }
+  # let!(:tag) { create(:tag, name:'hoge') }
   before do
+    @tag = FactoryBot.create(:tag)
     visit new_admin_tag_path
   end
   context '表示の確認' do
@@ -15,26 +16,20 @@ describe '新規タグ作成画面(new_admin_tag_path)のテスト' do
       expect(page).to have_button '作成'
     end
     it 'タグ一覧があるか' do
-      expect(page).to have_content tag.name
+      expect(page).to have_content @tag.name
     end
     it 'タグ削除リンクがあるか' do
       expect(page).to have_link '削除'
     end
   end
-  context 'タグ削除のテスト' do
-    it 'タグ削除' do
-      expect{ tag.destroy }.to change{ Tag.count }.by(-1)
+  context '処理の確認' do
+    it 'タグ作成後のリダイレクト先は正しいか' do
+      fill_in 'name', with: Faker::Lorem.characters(number:5)
+      click_button '作成'
+      expect(page).to have_current_path new_admin_tag_path
     end
-  end
-end
-
-describe '作成処理のテスト' do
-  before do
-    visit new_admin_tag_path
-  end
-  it '作成後のリダイレクト先は正しいか' do
-    fill_in 'name', with: Faker::Lorem.characters(number:5)
-    click_button '作成'
-    expect(page).to have_current_path new_admin_tag_path
+    it 'タグ削除が正しくできるか' do
+      expect{ @tag.destroy }.to change{ Tag.count }.by(-1)
+    end
   end
 end
