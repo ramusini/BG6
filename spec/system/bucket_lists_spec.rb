87e326b4a76add9949977b7be3df841dcb5feb29
ramusini <ramusini@gmail.com>
2022-08-25 14:59:19 +0000

    Rspec

---
+++ spec/system/bucket_lists_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'bucket_listコントローラーのテスト' do
+  context 'new,createの確認' do
+  end
+  context 'indexの確認' do
+  end
+  context 'showの確認' do
+  end
+  context 'edit,destroyの確認' do
+  end
+end
